31d2bcc791b95f6368af75e3666295008ccc648e
xshi <stushi0107@gmail.com>
2023-04-08 10:30:24 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         System.out.println("Hello world!");
+    }
+}
+
+class IfTest {
+    public static void main (String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x is not 3");
+        }
+        System.out.println("This runs no matter what");
     }
 }
