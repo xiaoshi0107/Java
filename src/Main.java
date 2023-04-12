8646eafda92f942cee5d321f8463de7f3d28973d
xshi <stushi0107@gmail.com>
2023-04-12 20:06:34 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -15,3 +15,51 @@ class IfTest {
         System.out.println("This runs no matter what");
     }
 }
+
+class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff! Ruff!");
+    }
+}
+
+class DogTestDrive {
+    public static void main (String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
+
+class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Playing the movie");
+    }
+}
+
+class MovieTestDrive {
+    public static void main (String[] args) {
+        Movie one = new Movie();
+        one.title = "Gone with the stock";
+        one.genre = "Tragic";
+        one.rating = -2;
+
+        Movie two = new Movie();
+        two.title = "Lost in Cubicle Space";
+        two.genre = "Comedy";
+        two.rating = 5;
+        two.playIt();
+
+        Movie three = new Movie();
+        three.title =  "Byte Club";
+        three.genre = "Tragic but ultimately uplifting";
+        three.rating = 127;
+    }
+}
